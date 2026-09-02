30f8f934d45993fcaf927b9a58885bca89376406
aielu0000-svg <aielu0000@gmail.com>
2026-09-02 13:44:25 +0900

    初回利用ガイドをWebルートへ組み込み

--- web/src/client/main.tsx
+++ web/src/client/main.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App.js";
+import { FirstUseGuideController } from "./FirstUseGuide.js";
 import "./styles.css";
 import "./evidence-followup.css";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
+    <FirstUseGuideController />
   </React.StrictMode>,
 );
